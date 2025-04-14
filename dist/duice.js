2e5f3bbebeab84ac200ea779e4207c163bb48cfa
chomookun <chomookun@gmail.com>
2025-04-14 22:37:00 +0900

    css: updates img max-width

--- dist/duice.js
+++ dist/duice.js
@@ -1,5 +1,5 @@
 /*!
- * duice - v0.3.9
+ * duice - v0.3.11
  * git: https://gitbub.com/chomookun/duice
  * website: https://duice.chomookun.com
  * Released under the LGPL(GNU Lesser General Public License version 3) License
